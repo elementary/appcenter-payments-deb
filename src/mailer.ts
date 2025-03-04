ea6e87d08882580eb8f3d8689a0e900f28db1f3a
Lewis Goddard <lewis.goddard@eustasy.org>
2025-03-04 08:13:57 +0000

    Update mailer.ts

--- src/mailer.ts
+++ src/mailer.ts
@@ -14,10 +14,10 @@ export async function sendReceipt (request: RequestType): Promise<Boolean> {
   form.append('v:amount', Math.round(request.amount / 100))
   form.append('v:rdnn', request.rdnn)
 
-  const res = await fetch(`https://api.mailgun.net/v3/mg.elementary.io/messages`, {
+  const res = await fetch('https://api.mailgun.net/v3/mg.elementary.io/messages', {
     method: 'POST',
     headers: {
-      'Authorization': 'Basic ' + Buffer.from(`${MAILGUN_USERNAME}:${MAILGUN_API_KEY}`).toString('base64')
+      Authorization: 'Basic ' + Buffer.from(`${MAILGUN_USERNAME}:${MAILGUN_API_KEY}`).toString('base64')
     },
     // I hate TypeScript and npm _most_ days.
     body: ((form as unknown) as string)
